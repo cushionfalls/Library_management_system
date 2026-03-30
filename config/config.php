a4101ad5134d317559f4cd0673e2034f8583b0fc
cuhionfalls <np03cs4s250041@heraldcollege.edu.np>
2026-03-30 14:08:35 +0545

    minor changes

--- config/config.php
+++ config/config.php
@@ -15,7 +15,7 @@ define('DB_PORT', 3306);
 // Google Email Configuration (using App Password)
 define('MAIL_HOST', 'smtp.gmail.com');
 define('MAIL_PORT', 587);
-define('MAIL_USERNAME', 'manjilsapkota845@gmail.com'); // Change this to your Gmail
+define('MAIL_USERNAME', 'paper.otp.support@gmail.com'); // Change this to your Gmail
 define('MAIL_PASSWORD', 'oqzr prmh poog ykqe');     // Change this to your App Password
 define('MAIL_FROM', MAIL_USERNAME);
 define('MAIL_FROM_NAME', 'Library Management System');
